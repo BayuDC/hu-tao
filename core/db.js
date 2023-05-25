c4a13847f960879378609f260133030c2e990972
BayuDC <dev.bayudc@gmail.com>
2023-05-25 10:32:55 +0700

    feat: support custom db name

--- core/db.js
+++ core/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const mongouri = process.env.MONGO_URI;
+const dbName = process.env.MONGO_DB_NAME;
 
 module.exports = next => {
-    mongoose.connect(mongouri, { dbName: 'hu-tao' }).then(() => {
+    mongoose.connect(mongouri, { dbName }).then(() => {
         console.log('Connected to database!');
         next();
     });
